9aa1e8283bdd4b89bbb41a34efd73d9ebf343624
eternitvy <crrissant@gmail.com>
2026-03-11 15:26:45 +0700

    percobaan 1

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\UserModel;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function index() {
+        $data = [
+            'level_id' => 2,
+            'username' => 'manager_tiga',
+            'nama' => 'Manager 3',
+            'password' => Hash::make('12345')
+        ];
+        UserModel::create($data); // Simpan dulu
+        $user = UserModel::all(); // Baru ambil semua
+        return view('user', ['data' => $user]);
+    }
+}
